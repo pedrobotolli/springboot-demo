5b4ed973d63cd48ea0113f76c8d8552d034cf4f2
Pedro Botolli Santos <pedrobotolli.santos@gmail.com>
2024-09-12 12:27:05 -0300

    added compose file to create vault, created configuration class, added vault properties on the application.properties file

--- src/main/java/br/com/pedrobotolli/demo/DemoApplication.java
+++ src/main/java/br/com/pedrobotolli/demo/DemoApplication.java
@@ -1,13 +1,37 @@
 package br.com.pedrobotolli.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import br.com.pedrobotolli.demo.config.MyConfiguration;
 
 @SpringBootApplication
-public class DemoApplication {
+@EnableConfigurationProperties(MyConfiguration.class)
+public class DemoApplication implements CommandLineRunner {
+	private final MyConfiguration configuration;
+
+	public DemoApplication(MyConfiguration configuration) {
+		this.configuration = configuration;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+
+		Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
+		logger.info("----------------------------------------");
+		logger.info("Configuration properties");
+		logger.info("   example.username is {}", configuration.getUsername());
+		logger.info("   example.password is {}", configuration.getPassword());
+		logger.info("----------------------------------------");
 	}
 
 }
